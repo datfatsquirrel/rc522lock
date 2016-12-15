497499efde527e113ce7d954aff7fdabe32442ed
Samuel Lambert <201503225@bhasvic.ac.uk>
2016-12-15 15:39:12 +0000

    fixed alignment issues with logs table

---
+++ server/init.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+import sqlite3
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	return render_template('index.html', title = 'RFID Door Lock')
+
+@app.route('/logs')
+def logs():
+	conn = sqlite3.connect('main.db')
+	conn.text_factory = str
+	curs = conn.cursor()
+	uids = curs.execute(''' SELECT uid from logs''').fetchall()
+	times = curs.execute(''' SELECT time from logs''').fetchall()
+	listLen = len(uids)
+	return render_template('logs.html', title = 'View Logs', uids = uids, times = times, listLen = listLen)
+
+if __name__ == '__main__':
+	app.run(debug=True)
